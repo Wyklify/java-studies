9a38e5fb9f62e68847aacb280c38b3a45cd40d20
Wyklify Jhonson <wyklifyjhonson01@gmail.com>
2025-12-07 14:29:54 -0300

    Atualizado para a questão 5.14

--- capitulo5/exercicios/juros_composto/JurosComposto.java
+++ capitulo5/exercicios/juros_composto/JurosComposto.java
@@ -2,21 +2,32 @@ package juros_composto;
 
 public class JurosComposto {
 
-    static double jurosCompostoTempo(double valorAplicado, int tempoTranscorrido) {
+    static double jurosCompostoTempo(double valorAplicado, int tempoTranscorrido, double porcentagem) {
 
-        return valorAplicado * Math.pow(1 + 0.05, tempoTranscorrido);
+        return valorAplicado * Math.pow(1 + porcentagem, tempoTranscorrido);
     }
 
     public static void main(String[] args) {
 
-        System.out.println(String.format("%,.2f", jurosCompostoTempo(1000, 10)));
+        int tempoTrancorrido = 10;
+        
 
-        do
-        System.out.printf("%s%-20s%s %n", "Year", "Amount on deposit", "oi");
-        while (true);
+        for (int i = 1; i <= tempoTrancorrido; i++) {
+
+
+            System.out.print( i + " \t");
+
+            for (int j = 5; j <= 10; j++) {
+                  
+                double porcentagem = j / 100.0;
+
+               System.out.print(String.format("%,.2f" + "\t", jurosCompostoTempo(1000, i, porcentagem)));
+            }
+
+            System.out.println();
+        }
+
+        
     }
-
-
-    
 
 }
