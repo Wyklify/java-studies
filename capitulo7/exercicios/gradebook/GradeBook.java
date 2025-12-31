64e190ce7c4aba320c1534c1384b3352b1e1f16e
Wyklify Jhonson <wyklifyjhonson01@gmail.com>
2025-12-31 11:46:11 -0300

    GradeBook modificado com matriz

--- capitulo7/exercicios/gradebook/GradeBook.java
+++ capitulo7/exercicios/gradebook/GradeBook.java
@@ -3,70 +3,82 @@ package exercicios.gradebook;
 public class GradeBook {
 
     private String courseName;
-    private int[] grades;
+    private int[][] grades;
 
-
-    public GradeBook(String courseName, int[] grades) {
+    public GradeBook(String courseName, int[][] grades) {
         this.courseName = courseName;
         this.grades = grades;
     }
-
 
     public void setCourseName(String courseName) {
         this.courseName = courseName;
     }
 
-
     public String getCourseName() {
         return courseName;
     }
-
 
     public void processGrade() {
 
         outputGrades();
 
-        System.out.printf("\nClass average is %.2f%n", getAverage());
+        //System.out.printf("\nClass average is %.2f%n", getAverage());
 
         System.out.printf("Lowest grade is %d%nHighest grade is %d%n%n", getMinimum(), getMaximum());
 
         outputBarChart();
-        
+
     }
 
+    public void outputGrades() {
 
-    public  void outputGrades() {
-       
         System.out.printf("The grade are: %n%n");
+        System.out.print("             ");
+
+        for (int i = 0; i < grades[0].length; i++) {
+            System.out.printf("Test %d  ", i + 1);
+        }
+
+        System.out.println("Average");
 
         for (int i = 0; i < grades.length; i++) {
-            System.out.printf("Student %2d: %3d%n", i + 1, grades[i]);
+            System.out.printf("Student %2d", i + 1);
+
+            for (int test : grades[i]) {
+                System.out.printf("%8d",test);
+            }
+
+            double average = getAverage(grades[i]);
+            System.out.printf("%9.2f%n", average);
         }
     }
 
-
     public void outputBarChart() {
-       
+
         System.out.println("Grade distribution: ");
 
         int[] frequency = new int[11];
 
-        for (int grade : grades) {
-            
-            ++frequency[grade/10];
+        for (int[] studentGrades : grades) {
+
+            for (int grade : studentGrades) {
+
+                ++frequency[grade / 10];
+            }
+
         }
 
         for (int i = 0; i < frequency.length; i++) {
-            
+
             if (i == 10) {
-                
+
                 System.out.printf("%5d: ", 100);
             } else {
                 System.out.printf("%02d-%02d: ", i * 10, i * 10 + 9);
             }
 
             for (int j = 0; j < frequency[i]; j++) {
-                
+
                 System.out.print("*");
             }
 
@@ -74,50 +86,51 @@ public class GradeBook {
         }
     }
 
+    public double getAverage(int[] setOfGrades) {
 
-    public double  getAverage() {
-        
         int total = 0;
 
-        for (int grade : grades) {
-            
+        for (int grade : setOfGrades) {
             total += grade;
         }
 
-        return (double) total / grades.length;
+        return (double) total / setOfGrades.length;
     }
 
+    public int getMaximum() {
 
-    public  int getMaximum() {
-        
-        int highGrade = grades[0];
+        int highGrade = grades[0][0];
 
-        for (int grade : grades) {
-            
-            if (grade > highGrade) {
-                highGrade = grade;
+        for (int[] studentGrades : grades) {
+
+            for (int grade : studentGrades) {
+
+                if (grade > highGrade) {
+                    highGrade = grade;
+                }
             }
         }
 
         return highGrade;
     }
 
-
     public int getMinimum() {
-        
-        int lowGrade = grades[0];
 
-        for (int grade : grades) {
-            
-            if (grade < lowGrade) {
-                
-                lowGrade = grade;
+        int lowGrade = grades[0][0];
+
+        for (int[] studentGrades : grades) {
+
+            for (int grade : studentGrades) {
+
+                if (grade < lowGrade) {
+
+                    lowGrade = grade;
+                }
             }
+
         }
 
         return lowGrade;
     }
 
-    
-    
 }
